457d2c5a63f5f71b8f14051e49fb221710627bf0
sugamasaki <80560990+sugamasaki@users.noreply.github.com>
2024-05-09 17:34:40 +0900

    Update StateTree.cs まだべた書きだけど、一応動くバージョン

--- StateTree.cs
+++ StateTree.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -9,6 +10,7 @@ using System.Runtime.CompilerServices;
 using VMS.TPS.Common.Model.API;
 using VMS.TPS.Common.Model.Types;
 using System.Collections;
+using System.Xml.Linq;
 
 [assembly: AssemblyVersion("1.0.0.1")]
 
@@ -32,20 +34,54 @@ namespace VMS.TPS
     public partial class StateTree : Window
     {
         private List<Type> valueType = new List<Type>() { typeof(bool), typeof(sbyte), typeof(byte), typeof(short), typeof(ushort), typeof(int), typeof(uint), typeof(long), typeof(ulong), typeof(char), typeof(float), typeof(double), typeof(decimal), typeof(string) };
+        XElement xmlAPI;
+        private TextBox textBox;
         Object GetPropertyValue(Object obj, string name)
         {
             var property = obj.GetType().GetProperty(name);
             return property.GetValue(obj);
         }
-        public StateTree(Object obj)
+        public StateTree(ScriptContext context)
         {
-            TreeView treeView = CreateTreeView(obj);
+            string rtmPath = @"C:\Program Files (x86)\Varian\RTM\";
+            if (!Directory.Exists(rtmPath))
+            {
+                rtmPath = @"C:\Program Files\Varian\RTM\";
+            }
+            string[] versionPaths = Directory.GetDirectories(rtmPath);
+            foreach (var versionPath in versionPaths)
+            {
+                if (context.VersionInfo.Contains(versionPath.Split('\\').Last()))
+                {
+                    xmlAPI = XElement.Load(versionPath + @"\esapi\API\VMS.TPS.Common.Model.API.xml");
+                }
+            }
+            TreeView treeView = CreateTreeView(context);
             InitializeComponent(treeView);
         }
         private void InitializeComponent(TreeView treeView)
         {
             this.Title = "StateTree for ESAPI";
-            this.Content = treeView;
+            Grid grid = new Grid();
+            grid.ColumnDefinitions.Add(new ColumnDefinition());
+            grid.ColumnDefinitions.Add(new ColumnDefinition());
+            grid.ColumnDefinitions.Add(new ColumnDefinition());
+            grid.ColumnDefinitions[0].Width = new GridLength(2.0, GridUnitType.Star);
+            grid.ColumnDefinitions[1].Width = GridLength.Auto;
+            grid.ColumnDefinitions[2].Width = new GridLength(1.0, GridUnitType.Star);
+            treeView.SetValue(Grid.ColumnProperty, 0);
+            GridSplitter splitter = new GridSplitter();
+            splitter.SetValue(Grid.ColumnProperty, 1);
+            splitter.Width = 5;
+            splitter.HorizontalAlignment = HorizontalAlignment.Center;
+            textBox = new TextBox();
+            textBox.VerticalScrollBarVisibility = ScrollBarVisibility.Visible;
+            textBox.TextWrapping = TextWrapping.Wrap;
+            textBox.SetValue(Grid.ColumnProperty, 2);
+            grid.Children.Add(treeView);
+            grid.Children.Add(splitter);
+            grid.Children.Add(textBox);
+            this.Content = grid;
         }
         private TreeView CreateTreeView(Object obj)
         {
@@ -62,6 +98,7 @@ namespace VMS.TPS
             item.Tag = obj;
             item.Items.Add("Loading..."); // dummy
             item.Expanded += OnTreeViewItemExpanded;
+            item.Selected += OnItemSelected;
             return item;
         }
 
@@ -84,12 +121,20 @@ namespace VMS.TPS
                 Object obj = item.Tag;
                 FieldInfo field = item.Items[0] as FieldInfo;
                 item.Items.Clear();
+                item.Tag = field;
                 try
                 {
                     var value = field.GetValue(obj);
                     if (value.GetType() != obj.GetType())
                     {
-                        CreateFildOrPropertyItem(value, field.Name, item);
+                        if(field.IsInitOnly || field.IsLiteral)
+                        {
+                            CreateFildOrPropertyItem(value, field, item, true, false);
+                        }
+                        else
+                        {
+                            CreateFildOrPropertyItem(value, field, item);
+                        }
                     }
 
                 }
@@ -99,11 +144,11 @@ namespace VMS.TPS
                 }
                 catch (TargetInvocationException)
                 {
+                    item.Header = item.Header.ToString() + ": null";
                 }
                 catch (TargetParameterCountException)
                 {
                     item.Header = item.Header.ToString() + " TargetParameterCountException Error?";
-                    //parentItem.Items.Remove(item);
                 }
                 catch (Exception ex)
                 {
@@ -120,12 +165,13 @@ namespace VMS.TPS
                 Object obj = item.Tag;
                 PropertyInfo property = item.Items[0] as PropertyInfo;
                 item.Items.Clear();
+                item.Tag = property;
                 try
                 {
                     if (property.GetIndexParameters().Length == 0)
                     {
                         var value = property.GetValue(obj);
-                        CreateFildOrPropertyItem(value, property.Name, item, property.CanRead, property.CanWrite);
+                        CreateFildOrPropertyItem(value, property, item, property.CanRead, property.CanWrite);
                     }
                     else if (property.GetIndexParameters().Length == 1)
                     {
@@ -137,9 +183,11 @@ namespace VMS.TPS
                             {
                                 var item1 = new TreeViewItem();
                                 item1.Header = property.Name + i.ToString();
-                                item.Items.Add(item1);
                                 var value = property.GetValue(obj, new object[] { i });
-                                CreateFildOrPropertyItem(value, property.Name, item1, property.CanRead, property.CanWrite);
+                                item1.Tag = value;
+                                item1.Selected += OnItemSelected;
+                                item.Items.Add(item1);
+                                CreateFildOrPropertyItem(value, property, item1, property.CanRead, property.CanWrite);
                             }
                         }
                         else if (parameters[0].ParameterType == typeof(string))
@@ -150,9 +198,11 @@ namespace VMS.TPS
                             {
                                 var item1 = new TreeViewItem();
                                 item1.Header = property.Name + count.ToString();
-                                item.Items.Add(item1);
                                 var value = property.GetValue(obj, new object[] { key });
-                                CreateFildOrPropertyItem(value, property.Name, item1, property.CanRead, property.CanWrite);
+                                item1.Tag = value;
+                                item1.Selected += OnItemSelected;
+                                item.Items.Add(item1);
+                                CreateFildOrPropertyItem(value, property, item1, property.CanRead, property.CanWrite);
                                 count++;
                             }
                         }
@@ -172,11 +222,11 @@ namespace VMS.TPS
                 }
                 catch (TargetInvocationException)
                 {
+                    item.Header = item.Header.ToString() + ": null";
                 }
                 catch (TargetParameterCountException)
                 {
-                    item.Header = item.Header.ToString() + " TargetParameterCountException Error?";
-                    //parentItem.Items.Remove(item);
+                    item.Header = item.Header.ToString() + " Error! TargetParameterCountException";
                 }
                 catch (Exception ex)
                 {
@@ -184,6 +234,162 @@ namespace VMS.TPS
                 }
             }
 
+        }
+        private void OnItemSelected(object sender, RoutedEventArgs e)
+        {
+            TreeViewItem item = sender as TreeViewItem;
+            textBox.Text = "";
+            if (item.Tag == null)
+            {
+                e.Handled = true;
+                return;
+            }
+            if (item.Items.Count == 1 && (item.Items[0] is PropertyInfo || item.Items[0] is FieldInfo))
+            {
+                string name = "";
+                if (item.Items[0] is PropertyInfo)
+                {
+                    name = ((PropertyInfo)item.Items[0]).PropertyType.FullName;
+                }
+                else if (item.Items[0] is FieldInfo)
+                {
+                    name = ((FieldInfo)item.Items[0]).FieldType.FullName;
+
+                }
+                IEnumerable<XElement> members = xmlAPI.Element("members").Elements("member");
+                if (members.Any(m => m.Attribute("name").Value.EndsWith(name)))
+                {
+                    XElement member = members.Single(m => m.Attribute("name").Value.EndsWith(name));
+                    foreach (var attribute in member.Attributes())
+                    {
+                        textBox.Text += attribute.Value;
+                    }
+                    foreach (var element in member.Elements())
+                    {
+                        textBox.Text += element.Value;
+                    }
+                }
+                else
+                {
+                    TreeViewItem parentItem = item.Parent as TreeViewItem;
+                    if (parentItem.Tag is PropertyInfo)
+                    {
+                        name = ((PropertyInfo)parentItem.Tag).PropertyType.FullName + "." + item.Header;
+                    }
+                    else if (parentItem.Tag is FieldInfo)
+                    {
+                        name = ((FieldInfo)parentItem.Tag).FieldType.FullName + "." + item.Header;
+                    }
+                    else
+                    {
+                        name = parentItem.Tag.GetType().FullName + "." + item.Header;
+                    }
+                    if (members.Any(m => m.Attribute("name").Value.EndsWith(name)))
+                    {
+                        XElement member = members.Single(m => m.Attribute("name").Value.EndsWith(name));
+                        foreach (var attribute in member.Attributes())
+                        {
+                            textBox.Text += attribute.Value;
+                        }
+                        foreach (var element in member.Elements())
+                        {
+                            textBox.Text += element.Value;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                string name = "";
+                if (item.Tag is PropertyInfo)
+                {
+                    name = ((PropertyInfo)item.Tag).PropertyType.FullName;
+                }
+                else if (item.Tag is FieldInfo)
+                {
+                    name = ((FieldInfo)item.Tag).FieldType.FullName;
+
+                }
+                else if (item.Tag is MethodInfo)
+                {
+                    MethodInfo method = item.Tag as MethodInfo;
+                    if (method.GetParameters().Length == 0)
+                    {
+                        name += string.Format(".{0}()", method.Name);
+                    }
+                    else
+                    {
+                        foreach (var p in method.GetParameters())
+                        {
+                            if (name == "")
+                            {
+                                name += string.Format(".{0}({1}", method.Name, p.ParameterType.FullName);
+                            }
+                            else
+                            {
+                                name += string.Format(",{0}",p.ParameterType.FullName);
+                            }
+                        }
+                        name += ")";
+                    }
+                    TreeViewItem parentItem = item.Parent as TreeViewItem;
+                    if (parentItem.Tag is PropertyInfo)
+                    {
+                        name = ((PropertyInfo)parentItem.Tag).PropertyType.FullName + name;
+                    }
+                    else if (parentItem.Tag is FieldInfo)
+                    {
+                        name = ((FieldInfo)parentItem.Tag).FieldType.FullName + name;
+                    }
+                }
+                else
+                {
+                    name = item.Tag.GetType().FullName;
+                }
+                IEnumerable<XElement> members = xmlAPI.Element("members").Elements("member");
+                if (members.Any(m => m.Attribute("name").Value.EndsWith(name)))
+                {
+                    XElement member = members.Single(m => m.Attribute("name").Value.EndsWith(name));
+                    foreach (var attribute in member.Attributes())
+                    {
+                        textBox.Text += attribute.Value;
+                    }
+                    foreach (var element in member.Elements())
+                    {
+                        textBox.Text += element.Value;
+                    }
+                }
+                else
+                {
+                    TreeViewItem parentItem = item.Parent as TreeViewItem;
+                    if (parentItem.Tag is PropertyInfo)
+                    {
+                        name = ((PropertyInfo)parentItem.Tag).PropertyType.FullName + "." + item.Header;
+                    }
+                    else if (parentItem.Tag is FieldInfo)
+                    {
+                        name = ((FieldInfo)parentItem.Tag).FieldType.FullName + "." + item.Header;
+                    }
+                    else
+                    {
+                        name = parentItem.Tag.GetType().FullName + "." + item.Header;
+                    }
+                    if (members.Any(m => m.Attribute("name").Value.EndsWith(name)))
+                    {
+                        XElement member = members.Single(m => m.Attribute("name").Value.EndsWith(name));
+                        foreach (var attribute in member.Attributes())
+                        {
+                            textBox.Text += attribute.Value;
+                        }
+                        foreach (var element in member.Elements())
+                        {
+                            textBox.Text += element.Value;
+                        }
+                    }
+                }
+
+            }
+            e.Handled = true;
         }
         private void CreateItems(Object obj, TreeViewItem parentItem)
         {
@@ -206,6 +412,7 @@ namespace VMS.TPS
                 item.Tag = obj;
                 item.Items.Add(field);
                 item.Expanded += OnFieldItemExpanded;
+                item.Selected += OnItemSelected;
                 parentItem.Items.Add(item);
 
             }
@@ -220,11 +427,21 @@ namespace VMS.TPS
                 item.Tag = obj;
                 item.Items.Add(property);
                 item.Expanded += OnPropertyItemExpanded;
+                item.Selected += OnItemSelected;
                 parentItem.Items.Add(item);
             }
         }
-        private void CreateFildOrPropertyItem(Object value, string name, TreeViewItem parentItem, bool canGet = true, bool canSet = true)
+        private void CreateFildOrPropertyItem(Object value, Object info, TreeViewItem parentItem, bool canGet = true, bool canSet = true)
         {
+            string name = "";
+            if (info is PropertyInfo)
+            {
+                name = ((PropertyInfo)info).Name;
+            }
+            else if(info is FieldInfo)
+            {
+                name = ((FieldInfo)info).Name;
+            }            
             var type = value.GetType();
             if (type.IsArray)
             {
@@ -238,6 +455,8 @@ namespace VMS.TPS
                 {
                     var item = new TreeViewItem();
                     item.Header = string.Format("{0}[{1}]", name, count);
+                    item.Tag = v;
+                    item.Selected += OnItemSelected;
                     parentItem.Items.Add(item);
                     count++;
                     if (valueType.Contains(v.GetType()))
@@ -280,6 +499,8 @@ namespace VMS.TPS
                     continue;
                 }
                 var item = new TreeViewItem();
+                item.Tag = method;
+                item.Selected += OnItemSelected;
                 item.Header = "";
                 if (method.GetParameters().Length == 0)
                 {
@@ -297,8 +518,8 @@ namespace VMS.TPS
                         {
                             item.Header = string.Format("{0}, {1} {2}", item.Header, p.ParameterType.Name, p.Name);
                         }
-                        item.Header = item.Header.ToString() + ")";
                     }
+                    item.Header = item.Header.ToString() + ")";
                 }
                 parentItem.Items.Add(item);
             }
@@ -315,6 +536,7 @@ namespace VMS.TPS
                     int[] indices = new int[arr.Rank];
                     var strs = name.Split(',');
                     item.Header = strs[0] + "[";
+                    item.Selected += OnItemSelected;
                     for (int j = 0; j < arr.Rank - 1; j++)
                     {
                         item.Header = string.Format("{0},{1}", item.Header, strs[j + 1]);
@@ -332,6 +554,7 @@ namespace VMS.TPS
                     parentItem.Items.Add(item);
                     if (valueType.Contains(arr.GetType().GetElementType()))
                     {
+                        item.Tag = arr.GetValue(indices);
                         CreateChildItems(arr.GetValue(indices), item, canGet, canSet);
                     }
                     else if (arr.GetType().GetElementType().IsArray)
@@ -340,6 +563,7 @@ namespace VMS.TPS
                     }
                     else
                     {
+                        item.Tag = arr.GetValue(indices);
                         CreateItems(arr.GetValue(indices), item);
                     }
                 }
@@ -369,6 +593,8 @@ namespace VMS.TPS
             type += "}";
             item1.Header = type;
             item2.Header = "value: " + obj.ToString();
+            item1.Selected += OnItemSelected;
+            item2.Selected += OnItemSelected;
             parentItem.Items.Add(item1);
             parentItem.Items.Add(item2);
         }
